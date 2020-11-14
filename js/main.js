2dc8ba3ac812932d56a6012d07c753b7a2ede749
Yifei-G <yifei.ge91@gmail.com>
2020-11-14 15:06:15 +0100

    added the logic to get todos from data.json and the logic of event listener
    Added the logic to query all the todos from data.json when the page is loading.
    
    Changed the way of adding a click event listener, preventing from adding unnecessary event listeners.

--- js/main.js
+++ js/main.js
@@ -1,32 +1,65 @@
-function checkTODOList(){
-    const todoCheckboxs = document.querySelectorAll(".todo-check");
-    const todoList = [...todoCheckboxs]
-    todoList.forEach(todo =>{
-        todo.addEventListener("click",()=>{
-            const todoItem = todo.nextElementSibling;
-            if (todo.checked){
-                todoItem.style.textDecorationLine = "line-through";
-            }
-            else{
-                todoItem.style.textDecorationLine = "none";
-            }
-            
-            
-        });
+import Request from "./request.js";
+
+function createTodoList(todos){
+    const todoList = document.querySelector("#todo-list");
+    todos.forEach(todo=>{
+        const newTodo = `<li> <input type="checkbox" class="todo-check">
+        <span class="todo-item">${todo}</span> </li>`;        
+        todoList.insertAdjacentHTML("beforeend",newTodo);
+    });
+
+    // after all the todos are in the DOM, get all of them and add click eventListener one by one.
+    // todoCheckboxes is nodeList not JS Array!!!
+    const todoCheckboxes = document.querySelectorAll(".todo-check");
+    todoCheckboxes.forEach(todoCheckbox => addClickEvent(todoCheckbox));
+}
+
+
+async function getTodos(){
+    const request = new Request();
+    request.setBaseURL("http://localhost:3000");
+    try{
+            const data = await request.loadTOdos("/to-dos");
+            createTodoList(data);
+    }catch(error){
+        console.log(error);
+    }
+
+}
+
+
+function addClickEvent(todoInput){
+    todoInput.addEventListener("click",()=>{
+        const todoItem = todoInput.nextElementSibling;
+        if (todoInput.checked){
+            todoItem.style.textDecorationLine = "line-through";
+        }
+        else{
+            todoItem.style.textDecorationLine = "none";
+        }
     });
 }
 
-function addNewToDo(){
+
+function addTodoOnClick(){
+    const addTodoButton = document.querySelector("#add-todo");
+    addTodoButton.addEventListener("click",addNewTodo);
+}
+
+function addNewTodo(){
     const userTodo = document.querySelector("#new-todo").value;
     if(userTodo){
         const todoList = document.querySelector("#todo-list");
         const newTodo = `<li> <input type="checkbox" class="todo-check">
         <span class="todo-item">${userTodo}</span> </li>`;
-
-        console.log(newTodo);
         todoList.insertAdjacentHTML("beforeend",newTodo);
+
+        //get all the checkboxes
+        const todoCheckboxes = document.querySelectorAll(".todo-check");
+        //only add the clickEvent to the last todo 
+        const lastTodo = todoCheckboxes[todoCheckboxes.length - 1];
+        addClickEvent(lastTodo);
         document.querySelector("#new-todo").value = "";
-        checkTODOList();
     }
     else{
         const errorOutput = document.querySelector("#error-message");
@@ -43,5 +76,6 @@ function inputOnfocus(){
     });
 }
 
-checkTODOList();
+getTodos();
+addTodoOnClick();
 inputOnfocus();
